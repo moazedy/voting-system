ae0bd3760a9904489c64512b0e3d0b26af18a674
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-05-23 17:51:32 -0400

    creating new concept in system, named contributor

---
+++ domain/models/contributor.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Contributor struct {
+	Id             uuid.UUID `json:"id"`
+	Name           string    `json:"name,omitempty"`
+	ContributeTime time.Time `json:"contribute_time"`
+	Deleted        bool      `json:"deleted"`
+	DeletedAt      time.Time `json:"deleted_at,omitempty"`
+	ElectionId     string    `json:"election_id"`
+	VotedAt        time.Time `json:"voted_at"`
+}
