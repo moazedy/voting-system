d9175f0e47da6e04f7fcc13be1d2798af141f689
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-05-23 17:12:09 -0400

    adding needed fields to election struct

--- domain/models/election.go
+++ domain/models/election.go
@@ -7,10 +7,20 @@ import (
 )
 
 type Election struct {
-	Id           uuid.UUID `json:"id"`
-	Title        string    `json:"title"`
-	CreationTime time.Time `json:"creation_time"`
-	StartTime    time.Time `json:"start_time"`
-	EndTime      time.Time `json:"end_time"`
-	HasEnded     bool      `json:"has_ended"`
+	Id                   uuid.UUID    `json:"id"`
+	Title                string       `json:"title"`
+	CreationTime         time.Time    `json:"creation_time"`
+	StartTime            time.Time    `json:"start_time"`
+	EndTime              time.Time    `json:"end_time"`
+	HasEnded             bool         `json:"has_ended"`
+	Type                 ElectionType `json"type"`
+	CandidatesCountLimit int          `json:"candidate_count_limit"`
 }
+
+type ElectionType int
+
+const (
+	PublicVotersData ElectionType = iota
+	PrivateVotersData
+	SimpleYesORNo
+)
