75aaf0e000e3385632069b802d99bd1cdd732768
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-05-23 17:52:55 -0400

    changing name of field VoterId to ContributorId in vote struct

--- domain/models/vote.go
+++ domain/models/vote.go
@@ -10,7 +10,7 @@ type Vote struct {
 	Id            uuid.UUID `json:"id"`
 	VoteTime      time.Time `json:"vote_time"`
 	CandidateId   string    `json:"candidate_id"`
-	VoterId       string    `json:"voter_id, omitempty"`
+	ContributorId string    `json:"contributor_id, omitempty"`
 	VoteValue     bool      `json:"vote_value"`
 	PrivateVoting bool      `json:"private_voting"`
 	Deleted       bool      `json:"deleted,omitempty"`
