a21895528048297df49d09c3ce331e757e184692
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-05-25 14:21:23 -0400

    implementing ReadElectionData in logic layer

--- logic/election.go
+++ logic/election.go
@@ -1,15 +1,19 @@
 package logic
 
 import (
+	"context"
 	"errors"
 	"time"
 	"voting-system/constants"
 	"voting-system/domain/models"
 	"voting-system/repository"
+
+	"github.com/google/uuid"
 )
 
 type ElectionLogic interface {
-	CreateNewElection(userId string, electionData models.Election) error
+	CreateNewElection(ctx context.Context, userId string, electionData models.Election) error
+	ReadElectionData(ctx context.Context, electionId string) (*models.Election, error)
 }
 
 type election struct {
@@ -20,11 +24,47 @@ func NewElectionLogic() ElectionLogic {
 	return new(election)
 }
 
-func (e *election) CreateNewElection(userId string, electionData models.Election) error {
+func (e *election) CreateNewElection(ctx context.Context, userId string, electionData models.Election) error {
 	if e.repo == nil {
 		e.repo = repository.NewElectionRepo()
 	}
 
 	electionData.CreatorId = userId
 	electionData.CreationTime = time.Now()
+
+	if err := electionData.Validate(); err != nil {
+		return err
+	}
+
+	if err := e.repo.SaveNewElection(ctx, electionData); err != nil {
+		return errors.New(constants.InternalServerError)
+	}
+
+	return nil
+}
+
+func (e *election) ReadElectionData(ctx context.Context, electionId string) (*models.Election, error) {
+	if err := electionIdValidate(electionId); err != nil {
+		return nil, err
+	}
+
+	wantedElection, err := e.repo.ReadElectionData(ctx, electionId)
+	if err != nil {
+		return nil, errors.New(constants.InternalServerError)
+	}
+
+	return wantedElection, nil
+}
+
+func electionIdValidate(Id string) error {
+	if Id == "" {
+		return errors.New(constants.InvalidElectionId)
+	}
+
+	_, err := uuid.Parse(Id)
+	if err != nil {
+		return errors.New(constants.InvalidElectionId)
+	}
+
+	return nil
 }
