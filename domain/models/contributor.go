a95dff0ca495fe5e255a667cd707e0b35462921a
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-06-09 14:31:18 -0400

    adding Validate method for contributor entity

--- domain/models/contributor.go
+++ domain/models/contributor.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"errors"
 	"time"
+	"voting-system/constants"
 
 	"github.com/google/uuid"
 )
@@ -18,4 +20,36 @@ type Contributor struct {
 
 type ContributorsCount struct {
 	Count int `json:"count"`
+}
+
+func (c *Contributor) Validate() error {
+	if err := c.NameValidation(); err != nil {
+		return err
+	}
+
+	if err := c.ElectionIdValidation(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Contributor) NameValidation() error {
+	if c.Name == "" {
+		return errors.New(constants.ContributorNameCanNotBeEmpty)
+	}
+
+	if len(c.Name) > constants.MximomContributorNameLength {
+		return errors.New(constants.CandidateNameIsLongerThanExpected)
+	}
+	return nil
+}
+
+func (c *Contributor) ElectionIdValidation() error {
+	_, err := uuid.Parse(c.ElectionId)
+	if err != nil {
+		return errors.New(constants.InvalidElectionId)
+	}
+
+	return nil
 }
