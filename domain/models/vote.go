23ef2c508c01f42ef5dd977faf52b07adbbf02e1
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-06-10 11:33:43 -0400

    adding contributorName to vote data in models

--- domain/models/vote.go
+++ domain/models/vote.go
@@ -9,15 +9,16 @@ import (
 )
 
 type Vote struct {
-	Id            uuid.UUID `json:"id"`
-	VoteTime      time.Time `json:"vote_time"`
-	CandidateId   string    `json:"candidate_id"`
-	ContributorId string    `json:"contributor_id, omitempty"`
-	VoteValue     bool      `json:"vote_value"`
-	PrivateVoting bool      `json:"private_voting"`
-	Deleted       bool      `json:"deleted,omitempty"`
-	DeletedAt     time.Time `json:"deleted_at,omitempty"`
-	ElectionId    string    `json:"election_id"`
+	Id              uuid.UUID `json:"id"`
+	VoteTime        time.Time `json:"vote_time"`
+	CandidateId     string    `json:"candidate_id"`
+	ContributorId   string    `json:"contributor_id,omitempty"`
+	ContributorName string    `json:"contributor_name,omitempty"`
+	VoteValue       bool      `json:"vote_value"`
+	PrivateVoting   bool      `json:"private_voting"`
+	Deleted         bool      `json:"deleted,omitempty"`
+	DeletedAt       time.Time `json:"deleted_at,omitempty"`
+	ElectionId      string    `json:"election_id"`
 }
 
 func (v *Vote) Validate() error {
