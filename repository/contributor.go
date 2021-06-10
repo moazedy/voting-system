53964bc374483be06fe6b6958b0f824e74f1cb6f
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-06-10 11:35:02 -0400

    adding IsContributionExists method to contributor repo and changing returning data of saveContributor

--- repository/contributor.go
+++ repository/contributor.go
@@ -14,13 +14,17 @@ import (
 // interface here
 type ContributorRepo interface {
 	// SaveNewContributor is creating new contributor entity in the db
-	SaveNewContributor(ctx context.Context, newContributor models.Contributor) error
+	SaveNewContributor(ctx context.Context, newContributor models.Contributor) (*models.Id, error)
 	// GetListOfContributorsInAnElection gets list of all contributors in an election
 	GetListOfContributorsInAnElection(ctx context.Context, electionId, order string, offset, limit int) ([]models.Contributor, error)
 	// ReadContributorData reads a contributor's data from db using receiving id
-	ReadContributorData(ctx context.Context, contributorId string) (*models.Contributor, error)
+	ReadContributorData(ctx context.Context, contributorMetaId string) (*models.Contributor, error)
 	// DeleteContributor deletes a contributor using given id
-	DeleteContributor(ctx context.Context, contributorId string) error
+	DeleteContributor(ctx context.Context, contributorMetaId string) error
+	// IsContributorExists checks for a specific participation of some contributor existance
+	IsContributorExists(ctx context.Context, contributorId, electionId string) (*bool, error)
+	// IsContributionExists checks on some contribution existance using meta id
+	IsContributionExists(ctx context.Context, contributorMetaId string) (*bool, error)
 }
 
 // contributor is a struct that represents contributor entity in repository layer and its the way we can access to repository methods of
@@ -33,15 +37,22 @@ func NewContributorRepo() ContributorRepo {
 	return new(contributor)
 }
 
-func (c *contributor) SaveNewContributor(ctx context.Context, newContributor models.Contributor) error {
-	_, err := DBS.Couch.Query(couchbaseQueries.SaveContributorQuery, &gocb.QueryOptions{
-		PositionalParameters: []interface{}{newContributor.Id, newContributor},
+func (c *contributor) SaveNewContributor(ctx context.Context, newContributor models.Contributor) (*models.Id, error) {
+	result, err := DBS.Couch.Query(couchbaseQueries.SaveContributorQuery, &gocb.QueryOptions{
+		PositionalParameters: []interface{}{newContributor.MetaId, newContributor},
 	})
 	if err != nil {
 		log.Println(" error in saving new contributor, error :", err.Error())
-		return err
+		return nil, err
 	}
-	return nil
+
+	var id models.Id
+	err = result.One(&id)
+	if err != nil {
+		log.Println("error on reading contributor id value, error: ", err.Error())
+		return nil, err
+	}
+	return &id, nil
 }
 
 func (c *contributor) GetListOfContributorsInAnElection(ctx context.Context, electionId, order string, offset, limit int) ([]models.Contributor, error) {
@@ -72,9 +83,9 @@ func (c *contributor) GetListOfContributorsInAnElection(ctx context.Context, ele
 	return contributors, nil
 }
 
-func (c *contributor) ReadContributorData(ctx context.Context, contributorId string) (*models.Contributor, error) {
+func (c *contributor) ReadContributorData(ctx context.Context, contributorMetaId string) (*models.Contributor, error) {
 	result, err := DBS.Couch.Query(couchbaseQueries.ReadContributorQuery, &gocb.QueryOptions{
-		PositionalParameters: []interface{}{contributorId},
+		PositionalParameters: []interface{}{contributorMetaId},
 	})
 	if err != nil {
 		log.Println("error in reading contributor data, error :", err.Error())
@@ -93,13 +104,67 @@ func (c *contributor) ReadContributorData(ctx context.Context, contributorId str
 	return &con, nil
 }
 
-func (c *contributor) DeleteContributor(ctx context.Context, contributorId string) error {
+func (c *contributor) DeleteContributor(ctx context.Context, contributorMetaId string) error {
 	_, err := DBS.Couch.Query(couchbaseQueries.DeleteContributorQuery, &gocb.QueryOptions{
-		PositionalParameters: []interface{}{contributorId},
+		PositionalParameters: []interface{}{contributorMetaId},
 	})
 	if err != nil {
 		log.Println(" error in deleting contributor, error :", err.Error())
 		return err
 	}
 	return nil
+}
+
+func (c contributor) IsContributorExists(ctx context.Context, contributorId, electionId string) (*bool, error) {
+	result, err := DBS.Couch.Query(couchbaseQueries.ContributorExistanceQuery, &gocb.QueryOptions{
+		PositionalParameters: []interface{}{contributorId, electionId},
+	})
+	if err != nil {
+		log.Println("error in query execution in contributor existance check. error :", err.Error())
+		return nil, err
+	}
+
+	var count models.ContributorsCount
+	var exists bool
+	err = result.One(&count)
+	if err != nil {
+		if err == gocb.ErrNoResult {
+			return &exists, nil
+		}
+		log.Println("error in reading contributor count data. error :", err.Error())
+		return nil, err
+	}
+
+	if count.Count > 0 {
+		exists = true
+	}
+
+	return &exists, nil
+}
+
+func (c contributor) IsContributionExists(ctx context.Context, contributorMetaId string) (*bool, error) {
+	result, err := DBS.Couch.Query(couchbaseQueries.ContributionExistanceQuery, &gocb.QueryOptions{
+		PositionalParameters: []interface{}{contributorMetaId},
+	})
+	if err != nil {
+		log.Println("error in query execution in contributor existance check. error :", err.Error())
+		return nil, err
+	}
+
+	var count models.ContributorsCount
+	var exists bool
+	err = result.One(&count)
+	if err != nil {
+		if err == gocb.ErrNoResult {
+			return &exists, nil
+		}
+		log.Println("error in reading contributor count data. error :", err.Error())
+		return nil, err
+	}
+
+	if count.Count > 0 {
+		exists = true
+	}
+
+	return &exists, nil
 }
