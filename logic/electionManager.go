21b893bf617a9a59ad8645721b77d95ee2ae3a3a
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-06-14 11:27:23 -0400

    creating Run method for electionManager in logic, to running ManageElections in a certain period of time

--- logic/electionManager.go
+++ logic/electionManager.go
@@ -5,12 +5,16 @@ import (
 	"log"
 	"sync"
 	"time"
+
+	"github.com/go-co-op/gocron"
 )
 
 type ElectionManager interface {
 	// ManageElctions is a method for ElectionManager worker to terminate expired elections and save running
 	// elections result, in db
 	ManageElections(ctx context.Context) error
+	// Run is a method for ElectionManager to be executed periodically in a certain time
+	Run(periodTime int)
 }
 
 type electionManager struct {
@@ -21,7 +25,7 @@ func NewElectionManager() ElectionManager {
 	return new(electionManager)
 }
 
-func (e electionManager) ManageElections(ctx context.Context) error {
+func (e *electionManager) ManageElections(ctx context.Context) error {
 	// singlton design pattern ...
 	if e.electionLogic == nil {
 		e.electionLogic = NewElectionLogic()
@@ -37,6 +41,7 @@ func (e electionManager) ManageElections(ctx context.Context) error {
 	wg.Add(2 * len(elections))
 	// terminating expired elections and saving elction results in db
 	for _, v := range elections {
+		// a concurrent func for termination
 		go func() {
 			if time.Now().After(v.EndTime) {
 				v.HasEnded = true
@@ -44,6 +49,7 @@ func (e electionManager) ManageElections(ctx context.Context) error {
 			wg.Done()
 		}()
 
+		// another func to saving results
 		go func() {
 			_, err := e.electionLogic.CalculationElectionResults(ctx, v.Id.String(), "", true)
 			if err != nil {
@@ -55,4 +61,13 @@ func (e electionManager) ManageElections(ctx context.Context) error {
 	}
 
 	return nil
+}
+
+func (e *electionManager) Run(periodTime int) {
+	// using gocron package to create an object like a cronjob manager
+	s := gocron.NewScheduler(time.UTC)
+
+	// manager runs every (periodTime) * seconds, the passed function
+	s.Every(periodTime).Seconds().Do(e.ManageElections(context.Background()))
+	s.StartAsync()
 }
