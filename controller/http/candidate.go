9fb646c2f5dd18be0302ecb57c9de00cf6abc58a
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-06-14 15:49:52 -0400

    adding ReadCandidateData method to candidate controller

--- controller/http/candidate.go
+++ controller/http/candidate.go
@@ -10,7 +10,10 @@ import (
 )
 
 type CandidateController interface {
+	// AddNewCandidate adds new candidate to candidates list of an election
 	AddNewCandidate(ctx *gin.Context)
+	// ReadCandidateData gets candidate data of given candidate id
+	ReadCandidateData(ctx *gin.Context)
 }
 
 type candidate struct {
@@ -22,6 +25,7 @@ func NewCandidateController(logic logic.CandidateLogic) CandidateController {
 }
 
 func (c candidate) AddNewCandidate(ctx *gin.Context) {
+	// TODO : requester id should be extracted from user claims
 	requesterId := ""
 	var candidateData models.Candidate
 	err := ctx.BindJSON(&candidateData)
@@ -35,10 +39,34 @@ func (c candidate) AddNewCandidate(ctx *gin.Context) {
 	id, err := c.Logic.CreateNewCandidate(ctx, requesterId, candidateData)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error": constants.InternalServerError,
+			"error": err.Error(),
 		})
 		return
 	}
 
 	ctx.JSON(http.StatusCreated, id)
+}
+
+func (c candidate) ReadCandidateData(ctx *gin.Context) {
+	// TODO : requester id should be extracted from user claims
+	requesterId := ""
+	candidateId := ctx.Param("candidate_id")
+
+	err := logic.IdValidation(candidateId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	candidateData, err := c.Logic.ReadCandidateData(ctx, candidateId, requesterId, false)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, candidateData)
 }
