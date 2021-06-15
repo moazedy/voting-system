c9f2919bb9252b8a2a882c5550953bb728d7f10b
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-06-15 17:01:19 -0400

    adding ReadVoteData method to vote http controller

--- controller/http/vote.go
+++ controller/http/vote.go
@@ -12,6 +12,8 @@ import (
 type VoteController interface {
 	// SaveNewVote is a handler function to voting in an election
 	SaveNewVote(c *gin.Context)
+	// ReadVoteData reads data of a specific vote and returns it to requester
+	ReadVoteData(c *gin.Context)
 }
 
 type vote struct {
@@ -42,4 +44,27 @@ func (v vote) SaveNewVote(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, id)
+}
+
+func (v vote) ReadVoteData(c *gin.Context) {
+	// requesterId should be extracted from access token
+	requesterId := ""
+	voteId := c.Param("vote_id")
+	err := logic.IdValidation(voteId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	voteData, err := v.Logic.ReadVoteData(c, voteId, requesterId, false)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, voteData)
 }
