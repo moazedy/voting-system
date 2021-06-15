e1ee2fb42c955a0aa238b791c62cc605dd6ef54b
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-06-15 12:52:41 -0400

    adding GetListOfElectionCandidates function to candidate controller

--- controller/http/candidate.go
+++ controller/http/candidate.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"voting-system/constants"
 	"voting-system/domain/models"
+	"voting-system/helper"
 	"voting-system/logic"
 
 	"github.com/gin-gonic/gin"
@@ -18,6 +19,8 @@ type CandidateController interface {
 	DeleteCandidate(ctx *gin.Context)
 	// UpdateCandidate updates data of some specific candidate
 	UpdateCandidate(ctx *gin.Context)
+	// GetListOfElectionCandidates gets list of candidates of a specific election
+	GetListOfElectionCandidates(ctx *gin.Context)
 }
 
 type candidate struct {
@@ -130,4 +133,27 @@ func (c candidate) UpdateCandidate(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusNoContent, nil)
+}
+
+func (c candidate) GetListOfElectionCandidates(ctx *gin.Context) {
+	electionId := ctx.Param("election_id")
+	pg := newHttpPagination(helper.NewPagination(constants.PaginationMaxPerPage))
+	pagination := pg.FromGinContext(ctx)
+
+	err := logic.IdValidation(electionId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	candidates, err := c.Logic.GetListOfElectionCandidates(ctx, electionId, pagination)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, candidates)
 }
